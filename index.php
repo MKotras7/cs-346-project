b6eea92d9d81d60ac99ed27371a1735d8b9c8837
MKotras7 <mkotras7@gmail.com>
2021-11-06 18:27:04 -0500

    asd

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
     <body>
         <main>
             <h1 class="centeredHeader">Welcome to the Snake Game website! </h1>
-            <img style="width: 50%; aspect-ratio: 1;" alt="Animated gif of a game of snake being played" src="https://media0.giphy.com/media/RE5iREBNhI0Ok/giphy.gif?cid=790b761197c621b07bb63bccdf828f2d0065f6ea41f92345&rid=giphy.gif&ct=g"/>
-            <p>Snake Game is a multiplayer take on the classic game of snake. You can get started by creating an account to start playing games!</p>
-        </main>
+            <img style="float: left; width: 50%; aspect-ratio: 1;" alt="Animanted gif of a cute snake." src="https://c.tenor.com/onkjRbfOrLEAAAAC/snake-crawling.gif" />
+            <p style="margin-left: 10px;">
+                🐍 Snake Game is a multiplayer take on the classic game of snake. You can get started by creating an account to start playing games!
+            </p>
+            </main>
         <footer>
 
         </footer>
